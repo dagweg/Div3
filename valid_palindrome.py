9eaee5a3731359b9ad10a86340c525ca508006dd
dagweg <dagtef2@gmail.com>
2023-07-27 20:47:08 +0300

    First Commit

---
+++ valid_palindrome.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = s.lower()
+
+        word = ''
+        for c in s:
+            if 'a' <= c <= 'z' or '0' <= c <= '9':
+                word = word + c
+
+        n = len(word)
+        for i in range(n//2):
+            if word[i] != word[n-i-1]: 
+                return False
+
+        return True
