cb8fd22963e64d18cf05bd4485e49d1db5db35a3
dagweg <90281138+dagweg@users.noreply.github.com>
2023-07-27 21:02:05 +0300

    Create python_lists.py

---
+++ python_lists.py
@@ -0,0 +1,31 @@
+def main(N):
+    lst = []
+    for i in range(N):
+        line = input().split()
+        command = line[0]        
+        
+        if len(line) == 2:
+            line[1] = int(line[1])
+        elif len(line) == 3:
+            line[1] = int(line[1])
+            line[2] = int(line[2])
+        
+        if command == 'insert':
+            lst.insert(line[1],line[2])
+        elif command == 'print':
+            print(lst)
+        elif command == 'remove':
+            lst.remove(line[1])
+        elif command == 'append':
+            lst.append(line[1])
+        elif command == 'sort':
+            lst.sort()
+        elif command == 'pop':
+            lst.pop()
+        elif command == 'reverse':
+            lst.reverse()
+        
+
+if __name__ == '__main__':
+    N = int(input())
+    main(N)
