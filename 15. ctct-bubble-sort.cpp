0d3bd1feb691fc4f3f8ec5716fefce3ee9286809
Dagmawi Wegayehu <90281138+dagweg@users.noreply.github.com>
2023-08-16 21:37:11 +0300

    Create 15. ctct-bubble-sort.cpp

---
+++ 15. ctct-bubble-sort.cpp
@@ -0,0 +1,100 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'countSwaps' function below.
+ *
+ * The function accepts INTEGER_ARRAY a as parameter.
+ */
+
+void swap(int& a, int& b){
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+void countSwaps(vector<int> a) {
+    int tswap = 0;
+    int n = a.size();
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n-1; j++){
+            if(a[j] > a[j+1]){
+                tswap++;
+                swap(a[j], a[j+1]);
+            }
+        }
+    }
+    cout << "Array is sorted in " << tswap << " swaps." << endl;
+    cout << "First Element: " << a[0] << endl;
+    cout << "Last Element: " << a[n-1] << endl;
+    
+}
+
+int main()
+{
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    string a_temp_temp;
+    getline(cin, a_temp_temp);
+
+    vector<string> a_temp = split(rtrim(a_temp_temp));
+
+    vector<int> a(n);
+
+    for (int i = 0; i < n; i++) {
+        int a_item = stoi(a_temp[i]);
+
+        a[i] = a_item;
+    }
+
+    countSwaps(a);
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
