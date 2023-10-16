01cc3b946d97f35b69c40bede05325b53c08f209
Dagmawi Wegayehu <90281138+dagweg@users.noreply.github.com>
2023-10-16 18:38:41 +0300

    Create 26.linked-list-cycle-ii.py

---
+++ 26.linked-list-cycle-ii.py
@@ -0,0 +1,9 @@
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        s = set()
+        while head:
+            if head in s:
+                return head
+            s.add(head)
+            head = head.next
+        return None
